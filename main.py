96a4bc1e449c0d7051a77b9b99d73c38797da263
ahmetozmtn <ahmethack13@gmail.com>
2023-12-13 15:04:46 +0300

    feat: products route created

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for
+from models import Product, app, db
+import os
+import secrets
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/products")
+def products():
+    products = Product.query.all()
+    return render_template("products.html", products=products)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
